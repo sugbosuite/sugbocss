33abc308b6f63730161979f3ebf8c67604d228c1
Sugbo Software <allencasul.developer@gmail.com>
2026-04-05 17:10:06 +0800

    Add files via upload

---
+++ docs/assets/Accordion-BGYrChUo.js
@@ -0,0 +1,43 @@
+import{_ as o}from"./CodeBlock-SJHW0uCk.js";import{_ as c,c as d,a as l,b as t,d as s,e as r,i as n,F as m}from"./index-DY6dcnbJ.js";const p={class:"z-index-neg-1 pos-fixed xy-center top-50-pct left-50-pct w-100-vw h-100-vh pointer-events-none o-hidden lightpillar-bg color-scheme-dark"},u={class:"padding-top-4 padding-left-6 w-100-pct d-flex a-items-center gap-4 f-wrap-wrap"},f={class:"d-flex a-items-center gap-4"},b={class:"d-flex a-items-center gap-4"},g={class:"w-100-pct padding-top-16 padding-bottom-16 padding-left-6 padding-right-6 color-scheme-dark"},y={class:"d-grid row-gap-4 max-w-60-rem margin-auto"},x={class:"d-grid row-gap-8 row"},v={__name:"Accordion",setup(h){return(e,a)=>(l(),d(m,null,[t("div",p,[s(r,{topColor:"#0d6be6",bottomColor:"skyblue",intensity:.2,rotationSpeed:.5,glowAmount:.0012,pillarWidth:10,pillarHeight:.4,noiseIntensity:0,pillarRotation:100,interactive:!1,mixBlendMode:"normal"})]),t("header",null,[t("div",u,[t("div",f,[t("div",{onClick:a[0]||(a[0]=i=>e.$router.push("/docs")),class:"font-size-l text-placeholder-dark cursor-pointer"},"Docs"),a[2]||(a[2]=t("div",{class:"font-size-3xl text-placeholder-dark cursor-default"},"/",-1))]),t("div",b,[t("div",{onClick:a[1]||(a[1]=i=>e.$router.push("/components")),class:"font-size-l text-placeholder-dark cursor-pointer"},"Components"),a[3]||(a[3]=t("div",{class:"font-size-3xl text-placeholder-dark cursor-default"},"/",-1))]),a[4]||(a[4]=t("div",{class:"d-flex a-items-center gap-4"},[t("div",{class:"font-size-l text-description-dark cursor-default"},"Accordion")],-1))])]),t("main",g,[t("section",y,[a[7]||(a[7]=t("h1",{class:"text-primary"},"Accordion",-1)),a[8]||(a[8]=t("p",{class:"text-subtitle-dark weight-800"},"Overview",-1)),a[9]||(a[9]=t("p",{class:"max-w-45-rem padding-bottom-16"},"Accordion is a great way to organize and display content in a compact and interactive manner, allowing you to present content in collapsible sections.",-1)),t("div",x,[a[5]||(a[5]=n('<section class="max-w-25-rem shadow-primary" data-v-cc3fbfbe><details class="radius-16 border-bottom-default" data-v-cc3fbfbe><summary data-v-cc3fbfbe> Accordion 1 <span class="text-icon-dark font-size-9xl material-symbols-outlined animate-accordion-icon" data-v-cc3fbfbe>stat_minus_1</span></summary> A lightweight and responsive accordion component designed to organize content in collapsible sections. </details><details class="border-bottom-default" data-v-cc3fbfbe><summary data-v-cc3fbfbe> Accordion 2 <span class="text-icon-dark font-size-9xl material-symbols-outlined animate-accordion-icon" data-v-cc3fbfbe>stat_minus_1</span></summary> A sleek accordion UI widget featuring customizable styles and animations. Perfect for displaying FAQs, menus, or collapsible content with a professional touch. </details><details class="radius-15" data-v-cc3fbfbe><summary data-v-cc3fbfbe> Accordion 3 <span class="text-icon-dark font-size-9xl material-symbols-outlined animate-accordion-icon" data-v-cc3fbfbe>stat_minus_1</span></summary> A dynamic accordion component that allows users to expand or collapse multiple sections simultaneously. Ideal for enhancing user experience in data-heavy interfaces. </details></section>',1)),s(o,{class:"margin-bottom-8",code:`
+  <details class="radius-16 border-bottom-default">
+    <summary>
+      Accordion 1
+      <span class="text-icon-dark font-size-9xl material-symbols-outlined animate-accordion-icon">stat_minus_1</span>
+    </summary>
+    A lightweight and responsive accordion component designed to organize content in collapsible sections.
+  </details>
+  <details class="border-bottom-default">
+    <summary>
+      Accordion 2
+      <span class="text-icon-dark font-size-9xl material-symbols-outlined animate-accordion-icon">stat_minus_1</span>
+    </summary>
+    A sleek accordion UI widget featuring customizable styles and animations. Perfect for displaying FAQs, menus, or collapsible content with a professional touch.
+  </details>
+  <details class="radius-15">
+    <summary>
+      Accordion 3
+      <span class="text-icon-dark font-size-9xl material-symbols-outlined animate-accordion-icon">stat_minus_1</span>
+    </summary>
+    A dynamic accordion component that allows users to expand or collapse multiple sections simultaneously. Ideal for enhancing user experience in data-heavy interfaces.
+  </details>`}),a[6]||(a[6]=n('<section class="max-w-25-rem" data-v-cc3fbfbe><details class="bg-transparent border-bottom-default padding-left-0 padding-right-0" data-v-cc3fbfbe><summary data-v-cc3fbfbe> Accordion 1 <span class="text-icon-dark font-size-9xl material-symbols-outlined animate-accordion-icon" data-v-cc3fbfbe>stat_minus_1</span></summary> A lightweight and responsive accordion component designed to organize content in collapsible sections. </details><details class="bg-transparent border-bottom-default padding-left-0 padding-right-0" data-v-cc3fbfbe><summary data-v-cc3fbfbe> Accordion 2 <span class="text-icon-dark font-size-9xl material-symbols-outlined animate-accordion-icon" data-v-cc3fbfbe>stat_minus_1</span></summary> A sleek accordion UI widget featuring customizable styles and animations. Perfect for displaying FAQs, menus, or collapsible content with a professional touch. </details><details class="bg-transparent border-bottom-default padding-left-0 padding-right-0" data-v-cc3fbfbe><summary data-v-cc3fbfbe> Accordion 3 <span class="text-icon-dark font-size-9xl material-symbols-outlined animate-accordion-icon" data-v-cc3fbfbe>stat_minus_1</span></summary> A dynamic accordion component that allows users to expand or collapse multiple sections simultaneously. Ideal for enhancing user experience in data-heavy interfaces. </details></section>',1)),s(o,{class:"margin-bottom-8",code:`
+  <details class="bg-transparent border-bottom-default padding-left-0 padding-right-0">
+    <summary>
+      Accordion 1
+      <span class="text-icon-dark font-size-9xl material-symbols-outlined animate-accordion-icon">stat_minus_1</span>
+    </summary>
+    A lightweight and responsive accordion component designed to organize content in collapsible sections.
+  </details>
+  <details class="bg-transparent border-bottom-default padding-left-0 padding-right-0">
+    <summary>
+      Accordion 2
+      <span class="text-icon-dark font-size-9xl material-symbols-outlined animate-accordion-icon">stat_minus_1</span>
+    </summary>
+    A sleek accordion UI widget featuring customizable styles and animations. Perfect for displaying FAQs, menus, or collapsible content with a professional touch.
+  </details>
+  <details class="bg-transparent border-bottom-default padding-left-0 padding-right-0">
+    <summary>
+      Accordion 3
+      <span class="text-icon-dark font-size-9xl material-symbols-outlined animate-accordion-icon">stat_minus_1</span>
+    </summary>
+    A dynamic accordion component that allows users to expand or collapse multiple sections simultaneously. Ideal for enhancing user experience in data-heavy interfaces.
+  </details>`})])])])],64))}},_=c(v,[["__scopeId","data-v-cc3fbfbe"]]);export{_ as default};
